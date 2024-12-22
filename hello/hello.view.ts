055adce5dbe13e2cb702fedc10fbcaf7092e923c
frylo0 <frylo.d.ts@gmail.com>
2024-12-22 22:54:49 +0300

    feat: handle button click

--- hello/hello.view.ts
+++ hello/hello.view.ts
@@ -4,5 +4,11 @@ namespace $.$$ {
 			let name = this.name();
 			return name && `Hello, ${name}!`
 		}
+
+		on_button_click(event: MouseEvent) {
+			if (!(event.target instanceof HTMLElement)) return;
+
+			alert(`Clicked! ${event.target.id}`);
+		}
 	}
 }
